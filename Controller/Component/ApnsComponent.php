354849372d28b89f1083099724211b197958e059
Diosdado D. Campo <csicebu@gmail.com>
2012-11-21 18:21:35 +0800

    working component

--- Controller/Component/ApnsComponent.php
+++ Controller/Component/ApnsComponent.php
@@ -1,33 +1,44 @@
 <?php
 App::uses('Component', 'Controller');
-App::import('Vendor', 'ApnsPHP/Autoload');
+require_once(App::pluginPath('CakeApns') . 'Vendor' . DS . 'ApnsPHP' . DS .'Autoload.php');
 
 class ApnsComponent extends Component {
 	public $env;
 	public $app_cert_path;
 	public $entrust_cert_path;
+    public $combined_cert_path;
+    public $cert_passphrase;
+
 	public $identifier = 'CakeApns';
 	public $expiry = 30;
 
-	public function startup($controller) {
-		if(empty($this->env)) {
-			$this->env = (Configure::read('debug') > 0)
-				? ApnsPHP_Abstract::ENVIRONMENT_SANDBOX
-				: ApnsPHP_Abstract::ENVIRONMENT_PRODUCTION;
-		}
+    private function __loadConfig() {
+        $apns = Configure::read('CakeApns');
+        if(!$apns) throw new CakeException(__('Missing CakeApns configurations'));
+        if(!$apns['mode'] || !in_array($apns['mode'], array('sandbox', 'production')))
+            throw new CakeException(__('Valid modes are "sandbox" & "production" only'));
 
-		if(!file_exists($this->app_cert_path)) {
-			trigger_error("Certification at $this->app_cert_path does not exist.", E_USER_ERROR);
-		}
+        if(!$apns[$apns['mode']]['combined_certificates'] || empty($apns[$apns['mode']]['combined_certificates'])) 
+            throw new CakeException(__('Specify ' . $apns['mode'] . ' Combined Certificates'));
 
-		if(!file_exists($this->entrust_cert_path)) {
-			trigger_error("Certification at $this->entrust_cert_path does not exist.", E_USER_ERROR);
-		}
+        $this->combined_cert_path = $apns[$apns['mode']]['combined_certificates'];
+        $this->cert_passphrase = $apns[$apns['mode']]['cert_passphrase'];
+        
+        $this->env = $apns['mode']=='sandbox' 
+            ? ApnsPHP_Abstract::ENVIRONMENT_SANDBOX 
+            : ApnsPHP_Abstract::ENVIRONMENT_PRODUCTION;
+    }
+
+    public function startup($controller) {
+        $this->__loadConfig();
+        if(!file_exists($this->combined_cert_path)) {
+			throw new CakeException(__("Certification at $this->combined_cert_path does not exist."));
+        }
 	}
 
-	public function push($device_token, $text, $options = array()) {
-		$push = new ApnsPHP_Push($this->env, $this->app_cert_path);
-		$push->setRootCertificationAuthority($this->entrust_cert_path);
+    public function push($device_token, $text, $options = array()) {
+        $push = new ApnsPHP_Push($this->env, $this->combined_cert_path);
+        $push->setProviderCertificatePassphrase($this->cert_passphrase);
 
 		$push->connect();
 
@@ -54,7 +65,7 @@ class ApnsComponent extends Component {
 	}
 
 	public function feedback() {
-		$feedback = new ApnsPHP_Feedback($env, $this->app_cert_path);
+		$feedback = new ApnsPHP_Feedback($env, $this->combined_cert_path);
 		$feedback->connect();
 		$error = $feedback->receive();
 		$feedback->disconnect();
